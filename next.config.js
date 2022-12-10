ea9aa5d94b0520c0857379b86856fdbd317d8815
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-12-11 02:07:41 +0800

    Fix: disable image optimize

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : './',
   images: {
     domains: ['img1.doubanio.com', 'img2.doubanio.com', 'img3.doubanio.com', 'img4.doubanio.com', 'img5.doubanio.com', 'img6.doubanio.com', 'img7.doubanio.com', 'img8.doubanio.com', 'img9.doubanio.com'],
+    unoptimized: true,
   },
 }
 

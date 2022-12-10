4a0043199fae837687e8b9c803a51f6e30ef36c2
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-12-10 13:18:55 +0800

    UI: Update the ui
    dim color for checked book

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,13 @@ export default function Home() {
   }, [])
 
   return (
-    <div className='flex flex-col gap-4 px-6 py-4'>
-      <h1 className='text-xl font-bold'>豆瓣读书 {data.filter(book => book.check).length}/250</h1>
-      <div className='flex flex-col gap-2'>
+    <div className='flex flex-col gap-5 px-6 py-4'>
+      <h1 className='sr-only'>豆瓣250</h1>
+      <div className='whitespace-nowrap flex flex-col gap-2'>
         {data.map((book, i) =>
           <div key={i} className='w-96 flex items-center gap-4'>
             <input type='checkbox' checked={book.check}
-              className='cursor-pointer w-3.5 h-3.5'
+              className='appearance-none cursor-pointer w-3.5 h-3.5 border checked:bg-neutral-200 transition-colors rounded-sm shrink-0'
               onChange={() => {
                 const newData = [...data]
                 newData[i].check = !newData[i].check
@@ -31,11 +31,12 @@ export default function Home() {
                 localStorage.setItem('book_list', JSON.stringify(newData))
               }}
             />
-            <div>{book.title}</div>
-            <div>{book.author}</div>
+            <div className={`${book.check && 'text-neutral-400'}`}>{book.title}</div>
+            <div className={`${book.check ? 'text-neutral-400' : 'text-neutral-600'}`}>{book.author}</div>
           </div>
         )}
       </div>
+      <div className='text-neutral-500 font-serif text-xl font-bold'>豆瓣250 <span className='text-neutral-600'>{data.filter(book => book.check).length}/250</span></div>
     </div>
   )
 }
